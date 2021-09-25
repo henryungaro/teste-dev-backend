b34c5cf8cddd12f7d8804ab23011fd0ce0302789
Henry Ungaro <henrycosta007@gmail.com>
2021-09-24 21:56:11 -0300

    Project Setup

---
+++ dev/healthrecords/src/main/java/devzone/healthrecords/HealthrecordsApplication.java
@@ -0,0 +1,13 @@
+package devzone.healthrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthrecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthrecordsApplication.class, args);
+	}
+
+}
