0265b4d822c90e88d2ed06af94dd057bb859b5b2
Henry Ungaro <henrycosta007@gmail.com>
2021-09-27 23:46:50 -0300

    Code Adjustments

--- dev/healthrecords/src/main/java/devzone/healthrecords/controller/ClientController.java
+++ dev/healthrecords/src/main/java/devzone/healthrecords/controller/ClientController.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Optional;
 import java.lang.Double;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,95 +26,67 @@ import devzone.healthrecords.repository.ClientRepository;
 @RestController
 @RequestMapping("/client")
 public class ClientController {
-	
+
 	SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyy");
 	Date date = new Date();
-	private static double e = 2.718281828459045235360287;
-	Comparator<ClientRisk> compareByScore = (ClientRisk o1, ClientRisk o2) -> Double.compare(o1.getScore(), o2.getScore());
+	Comparator<ClientRisk> compareByScore = (ClientRisk o1, ClientRisk o2) -> Double.compare(o1.getScore(),
+			o2.getScore());
 
-			
-	
 	@Autowired
 	ClientRepository repository;
-	
-	//Get a list of everyone
+
+	// Get a list of everyone
 	@GetMapping("/list")
-	public List<Client> list()
-	{
+	public List<Client> list() {
 		return repository.findAll();
 	}
-	
+
 	@GetMapping("/criticalList")
-	public List<ClientRisk> findClientRisk()
-	{
+	public List<ClientRisk> findClientRisk() {
 
 		List<Client> listClient = repository.findAll();
-	    List<ClientRisk> listRisk = new ArrayList<>();
-	    Client auxClient = new Client();
-	    if(listClient.size() < 10)
-	    {
-		    for(int i = 0; i < listClient.size(); i++)
-		    {
-		    ClientRisk auxRisk = new ClientRisk();
-		    auxClient = listClient.get(i);
-		    auxRisk.setId(auxClient.getId());
-		    auxRisk.setName(auxClient.getName());
-		    auxRisk.setScore((1 / (Math.pow( 1 + e,-(-2.8 + repository.sumDegree(auxRisk.getId())))))* 100 );
-		    listRisk.add(auxRisk);
-		    }
-	    }
-	    else
-	    {
-	    	for(int i = 0; i < 9; i++)
-		    {
-		    ClientRisk auxRisk = new ClientRisk();
-		    auxClient = listClient.get(i);
-		    auxRisk.setId(auxClient.getId());
-		    auxRisk.setName(auxClient.getName());
-		    auxRisk.setScore((1 / (Math.pow( 1 + e,-(-2.8 + repository.sumDegree(auxRisk.getId())))))* 100 );
-		    listRisk.add(auxRisk);
-		    }
-	    }
-	    Collections.sort(listRisk, compareByScore.reversed());
-	    return listRisk;
+		List<ClientRisk> listRisk = new ArrayList<>();
+		Client auxClient = new Client();
+		int lowerValue = (listClient.size() < 10) ? listClient.size() : 9;
+		for (int i = 0; i < lowerValue; i++) {
+			auxClient = listClient.get(i);
+			ClientRisk auxRisk = new ClientRisk(auxClient, repository.sumDegree(auxClient.getId()));
+			listRisk.add(auxRisk);
+		}
+
+		Collections.sort(listRisk, compareByScore.reversed());
+		return listRisk;
 	}
-	
-//sd = repository,sumDegree(id)
-//e = 2.718281828459045235360287
-//(1 / (1 + e ^ -(-2.8 + sd ))) * 100;
-//(1 / (Math.pow( 1 + e,-(-2.8 + sd ))) )* 100
-//(1/(1+Math.exp(2.8-sd))) * 100;
-	
+
 	@PostMapping("/create")
 	Client newClient(@RequestBody Client newClient) {
 		newClient.setRegisterDate(dateFormat.format(date));
-        return repository.save(newClient);
-    }
-	
+		return repository.save(newClient);
+	}
+
 	@PutMapping("/update/{id}")
-    Client update(@RequestBody Client client, @PathVariable Long id) {
-        client.setId(id);
-        client.setRegisterDate(repository.findDateById(id));
-        client.setUpdateDate(dateFormat.format(date));
-        return repository.save(client);
-    }
+	Client update(@RequestBody Client client, @PathVariable Long id) {
+		client.setId(id);
+		client.setRegisterDate(repository.findDateById(id));
+		client.setUpdateDate(dateFormat.format(date));
+		return repository.save(client);
+	}
 
-
-	//Search by person's id
+	// Search by person's id
 	@GetMapping("/find/{id}")
 	public Optional<Client> findById(@PathVariable("id") Long id) {
 		return repository.findById(id);
 	}
-	
-	//Search by person's name
+
+	// Search by person's name
 	@GetMapping("/findName/{name}")
 	public Optional<Client> findByName(@PathVariable("name") String name) {
 		return repository.findByName(name);
 	}
-	
-	//Remove by id
+
+	// Remove by id
 	@DeleteMapping("/remove/{id}")
-	public void deleteById(@PathVariable("id") Long id){
+	public void deleteById(@PathVariable("id") Long id) {
 		repository.deleteById(id);
 	}
 
--- dev/healthrecords/src/main/java/devzone/healthrecords/model/ClientRisk.java
+++ dev/healthrecords/src/main/java/devzone/healthrecords/model/ClientRisk.java
@@ -11,4 +11,15 @@ public class ClientRisk {
 	private Long id;
 	private String name;
 	private double score;
+	private static double e = 2.718281828459045235360287;
+	
+	public ClientRisk(Client client, double sumDegree)
+	{
+		this.id = client.getId();
+		this.name = client.getName();
+		double sd = sumDegree;
+		double eulerPowered = Math.pow(e, -(-2.8 + sd));
+		double finalResult = (1 /( 1 + eulerPowered)) * 100;
+		this.score = finalResult;
+	}
 }
